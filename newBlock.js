db17dd875286ff642dc91d3a45d0bab9ff16821c
ishankbahl <ishankbahl97@gmail.com>
2018-01-09 14:44:31 +0530

    added newBlock definition

---
+++ newBlock.js
@@ -0,0 +1,12 @@
+const createBlock = (lastBlock, data) => {
+    let newBlock = {
+        timestamp: new Date().getTime(),
+        data: data,
+        index: lastBlock.index + 1,
+        previousHash: lastBlock.hash
+    };
+
+    newBlock.hash = createHash(newBlock);
+
+    return newBlock;
+}
